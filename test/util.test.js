45c4895dbd2f19ae55ff03ddeeb6a41f6478ca86
TS22082 <ts22082@gmail.com>
2020-06-01 19:19:54 -0700

    add test for error

--- test/util.test.js
+++ test/util.test.js
@@ -6,4 +6,10 @@ describe("Add", () => {
       expect(add(2, 2)).toBe(4);
     });
   });
+
+  describe("Fail", () => {
+    it("Should throw an error if the first argument is not a number", () => {
+      expect(() => add("2", 2)).toThrowError();
+    });
+  });
 });
